659bda3c973a8873cffd00faf47f7bc643f4f959
skirthankumar <kirthan.bytedocker@gmail.com>
2024-06-21 18:30:52 +0530

    new-account page

--- pages/new-account.js
+++ pages/new-account.js
@@ -1,102 +1,263 @@
-import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+"use client";
 
-const NewAccount = () => {
+import React, { useState } from "react";
+
+const SignUpForm = () => {
+  const [formValues, setFormValues] = useState({
+    title: "",
+    fullName: "",
+    phoneNumber: "",
+    homeAddress: "",
+    emailId: "",
+    panNumber: "",
+    aadharNumber: "",
+    bankName: "",
+    bankBranch: "",
+    ifscCode: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+
+  const handleGenerateSignInDetails = () => {
+    // TO DO: Implement logic to generate sign in details
+    console.log("Generate sign in details clicked");
+  };
+
   return (
-    <Container className="my-4">
-      <Row className="justify-content-center">
-        <Col md={8}>
-          <div className="text-center mb-4">
-            <img src="/kenstone-capital.png" alt="Kenstone Capital" />
-          </div>
-          <Form>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="2" controlId="formPrefix">
-                <Form.Label>Mr/Ms/Mrs</Form.Label>
-                <Form.Control type="text" placeholder="Mr/Ms/Mrs" />
-              </Form.Group>
-              <Form.Group as={Col} md="10" controlId="formFullName">
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the full name"
-                />
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formEmail">
-                <Form.Label>Email - ID</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Please enter the Email - ID"
-                />
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="formPhoneNumber">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the phone number"
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="formPanNumber">
-                <Form.Label>PAN Number</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the PAN Number"
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="formAadharNumber">
-                <Form.Label>Aadhar Number</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the Aadhar Number"
-                />
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formHomeAddress">
-                <Form.Label>Home Address</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the full address"
-                />
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="formBankName">
-                <Form.Label>Bank Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the Bank name"
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="formBankBranch">
-                <Form.Label>Bank Branch</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the Branch Name"
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="formIfscCode">
-                <Form.Label>IFSC Code</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please enter the IFSC Code"
-                />
-              </Form.Group>
-            </Row>
-            <div className="text-center">
-              <Button variant="primary" type="submit">
+    <div style={{ fontFamily: "sans-serif" }}>
+      <div style={styles.page}>
+        <div style={styles.container}>
+          <img
+            src="https://s3-alpha-sig.figma.com/img/9276/7575/8c205aa8560a614b8d34a06a056f545e?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Cai7KTBtEEwU2-fy4qdz9kblexcgaORUw-usJqCn5QJqaRNGQZYs4nk73r-yxYGI2VaD-ye2WY9xbWf-8oPNbtH2W5-Ob6Eic1Ps38QYi8RZiqv8VeM1XjTXX798r~uojlrhiB0TEfSdEecEWsVWQTirfS~QxqRbhu34fxuVajpZWYBnzp4pP6cNaVyDmdC6CLHbjbFL3vvaWxdzzWa3ki2vP6yVK87Qbe-~vaOc4uvsWONog-ErjncfpdF8L59RZfS0u9yg92GhhSXcxyuFssblmkmFdziOBJbt-S2IG9sxQJYzaZ6sb~jpDAkac590HFpYJ0DrDu2W6PvHzs5QXg__" // Replace with your image path
+            alt="Logo"
+            style={styles.image}
+          />
+          <div style={styles.formContainer}>
+            <form style={styles.form}>
+              <div style={styles.formRow}>
+                <label style={{ ...styles.label, width: "20%" }}>
+                  Title:
+                  <select
+                    name="title"
+                    value={formValues.title}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                  >
+                    <option value="">Mr/Mrs/Ms</option>
+                    <option value="Mr">Mr</option>
+                    <option value="Mrs">Mrs</option>
+                    <option value="Ms">Ms</option>
+                  </select>
+                </label>
+                <label style={{ ...styles.label, width: "40%" }}>
+                  Full Name:
+                  <input
+                    type="text"
+                    name="fullName"
+                    value={formValues.fullName}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter Full Name"
+                  />
+                </label>
+                <label style={{ ...styles.label, width: "40%" }}>
+                  Email - ID:
+                  <input
+                    type="email"
+                    name="emailId"
+                    value={formValues.emailId}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the e-mail address"
+                  />
+                </label>
+              </div>
+              <div style={styles.formRow}>
+                <label style={{ ...styles.label, width: "50%" }}>
+                  Phone Number:
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    value={formValues.phoneNumber}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the Phone Number"
+                  />
+                </label>
+                <label style={{ ...styles.label, width: "25%" }}>
+                  Aadhar Number:
+                  <input
+                    type="text"
+                    name="aadharNumber"
+                    value={formValues.aadharNumber}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the Aadhar Number"
+                  />
+                </label>
+                <label style={{ ...styles.label, width: "25%" }}>
+                  Pan Number:
+                  <input
+                    type="text"
+                    name="panNumber"
+                    value={formValues.panNumber}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the Pan Number"
+                  />
+                </label>
+              </div>
+              <div style={styles.formRow}>
+                <label style={{ ...styles.label, width: "100%" }}>
+                  Home Address:
+                  <input
+                    type="text"
+                    name="homeAddress"
+                    value={formValues.homeAddress}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the Full Address"
+                  />
+                </label>
+              </div>
+              <div style={styles.formRow}>
+                <label style={{ ...styles.label, width: "30%" }}>
+                  Bank Name:
+                  <input
+                    type="text"
+                    name="bankName"
+                    value={formValues.bankName}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the Bank Name"
+                  />
+                </label>
+                <label style={{ ...styles.label, width: "40%" }}>
+                  Bank Branch:
+                  <input
+                    type="text"
+                    name="bankBranch"
+                    value={formValues.bankBranch}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="Please enter the Branch Name"
+                  />
+                </label>
+                <label style={{ ...styles.label, width: "30%" }}>
+                  IFSC Code:
+                  <input
+                    type="text"
+                    name="ifscCode"
+                    value={formValues.ifscCode}
+                    onChange={handleInputChange}
+                    style={styles.input}
+                    placeholder="please enter the IFSC Code"
+                  />
+                </label>
+              </div>
+              <button
+                type="button"
+                onClick={handleGenerateSignInDetails}
+                style={styles.button}
+              >
                 Generate Sign In Details
-              </Button>
-            </div>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default NewAccount;
+const styles = {
+  page: {
+    backgroundColor: "white",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "sans-serif", // Change to a regular 400 weight font
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "30.87px",
+    fontFamily: "sans-serif", // Inherit the font from parent (page)
+  },
+  image: {
+    width: "290.53px",
+    height: "113.16px",
+    marginBottom: "20px",
+  },
+  formContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    fontFamily: "sans-serif", // Inherit the font from parent (container)
+  },
+  formTitle: {
+    marginBottom: "30px",
+    fontFamily: "sans-serif", // Inherit the font from parent (formContainer)
+  },
+  form: {
+    width: "1289.45px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+    fontFamily: "sans-serif", // Inherit the font from parent (formContainer)
+    height: "549.87px",
+  },
+  formRow: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    fontFamily: "sans-serif", // Inherit the font from parent (formContainer)
+  },
+  label: {
+    color: "black",
+    fontWeight: "bold",
+    display: "flex",
+    flexDirection: "column",
+    paddingRight: "5px",
+    paddingLeft: "5px",
+    textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)",
+    fontFamily: "sans-serif", // Inherit the font from parent (formContainer)
+  },
+  input: {
+    width: "100%",
+    padding: "10px",
+    boxSizing: "border-box",
+    backgroundColor: "#6C819A",
+    border: "none",
+    color: "white",
+    borderRadius: "20px",
+    fontFamily: "sans-serif", // Inherit the font from parent (formContainer)
+    fontSize: "12px", // Default font size for input fields
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Box shadow added
+  },
+  button: {
+    marginTop: "20px",
+    padding: "10px 20px",
+    fontSize: "10px",
+    cursor: "pointer",
+    backgroundColor: "#023A51",
+    color: "white",
+    border: "none",
+    borderRadius: "50px",
+    fontFamily: "sans-serif", // Inherit the font from parent (formContainer)
+    fontWeight: "normal", // Ensure normal weight (400) is used
+    width: "197px",
+    height: "50px",
+    alignContent: "center",
+    alignSelf: "center",
+  },
+};
+
+export default SignUpForm;
