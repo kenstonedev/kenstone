6873cea1d99dfb6f188c810527b93a0f56660247
PranavRao18 <prnvraosrs@gmail.com>
2024-06-30 13:28:21 +0530

    basic authentication

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { auth, db } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
